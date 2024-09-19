0b0ce1e90b939f92363a0dd661ba0b85db40eb31
Yuriy Pilipenko <yurik2061@gmail.com>
2024-09-19 08:44:31 +0300

    cng path to /pages

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,12 +1,13 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
-
+ 
+console.log(process.env.GITHUB_ID)
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID || '',
-      clientSecret: process.env.GITHUB_SECRET || '',
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
     // ...add more providers here
   ],
