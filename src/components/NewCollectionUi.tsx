e3b30c6e5a74abc039392fee3ebbf8369a322490
Yuriy Pilipenko <yurik2061@gmail.com>
2024-10-26 21:38:56 +0300

    folding div

--- src/components/NewCollectionUi.tsx
+++ src/components/NewCollectionUi.tsx
@@ -4,28 +4,34 @@ import React, { useState } from 'react'
 import { CancelBtn } from './Button/Button'
 import { BiDownArrow } from 'react-icons/bi'
 import { AddNewCollectionForm } from './forms/AddNewCollectionForm'
+import {cn} from './../lib/utils'
 
 function NewCollectionUi() {
     const [open, setOpen] = useState(false)
   return (
-    <div className='w-full grid gap-2 uiWrap '>
-    <CancelBtn className='text-slate-200'
-        onClick={()=> setOpen(!open)}>
-            {open ? 'Fold' :'Add new collection'}
-            {open 
-                ?  <BiDownArrow  className='r180' /> 
-                : <BiDownArrow className=''/>
-                }
-    </CancelBtn>
-        <div className='folded'>
+
+		<div 
+		className= {cn('w-full grid gap-2 uiWrap', 
+			open ? 'h-auto' : 'h-[36px]'
+		)}
+		>
+			<CancelBtn className='text-slate-200'
+				onClick={()=> setOpen(!open)}>
+						{open ? 'Fold' :'Add new collection'}
+						{open
+								?  <BiDownArrow  className='r180' />
+								: <BiDownArrow className=''/>
+								}
+			</CancelBtn>
+			<div className={open ? 'opened' : 'folded'}>
 					{open && <h2 className='text-slate-300 text-center font-semibold'>
 							Add collection</h2>
 					}
 					{open &&(
-									 <AddNewCollectionForm />
+						<AddNewCollectionForm />
 									)}
-				</div>
-    </div>
+			</div>
+		</div>
   )
 }
 
