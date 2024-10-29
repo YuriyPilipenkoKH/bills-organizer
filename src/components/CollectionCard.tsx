86e868dee8555fd7b85d770e5494bcfd02e2f396
Yuriy Pilipenko <yurik2061@gmail.com>
2024-10-29 21:23:00 +0200

    preparing card layout

--- src/components/CollectionCard.tsx
+++ src/components/CollectionCard.tsx
@@ -1,5 +1,8 @@
 import { Bill, Collection } from '@prisma/client'
 import capitalize from '@/lib/capitalize'
+import { RiDeleteBin2Line } from "react-icons/ri";
+import { MdCallMissedOutgoing } from "react-icons/md";
+import { FlatBtn } from './Button/Button';
 
 interface CollectionCardProps {
     collection: Collection & {
@@ -9,8 +12,21 @@ interface CollectionCardProps {
 
 function CollectionCard({collection} :CollectionCardProps) {
   return (
-    <div className='card '>
-        {capitalize(collection?.name)}
+    <div className='card flex gap-2 items-center bg-cyan-700 border-slate-200 rounded-lg p-2 text-slate-400 '>
+			<h2 className='text-xl  font-bold w-24'>
+					{capitalize(collection?.name)}
+			</h2>
+			<span >
+				{collection.year}
+			</span>
+			<div className='flex gap-2  ml-auto'>
+				<FlatBtn >
+					<RiDeleteBin2Line />
+				</FlatBtn>
+				<FlatBtn >
+					<MdCallMissedOutgoing /> 
+				</FlatBtn>
+			</div>
     </div>
   )
 }
