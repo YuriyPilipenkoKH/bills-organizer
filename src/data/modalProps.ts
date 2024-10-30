ff3ccd9bd47697a9da4dccc7f9862dea6e41e282
Yuriy Pilipenko <yurik2061@gmail.com>
2024-10-30 10:46:06 +0200

    types & props for modal

---
+++ src/data/modalProps.ts
@@ -0,0 +1,38 @@
+import { ModalBaseTypes } from '@/types/modalTypes';
+
+export const DeletingCollectionConfirmProps: ModalBaseTypes = {
+  modalName: 'DeletingCollectionConfirm',
+  dimentions: ['320px', '180px'],
+  title: 'Are you sure deleting',
+  text: 'There will be no return....',
+};
+
+
+
+export const DeletingProductConfirmProps: ModalBaseTypes = {
+  modalName: 'DeletingProductConfirm',
+  dimentions: ['320px', '200px'],
+  title: 'Are you sure deleting',
+  text: 'There will be no return....',
+};
+
+export const EditProductProps: ModalBaseTypes = {
+  modalName: 'EditProduct',
+  dimentions: ['320px', '300px'],
+  title: 'Update product',
+  text: 'You can edit anything',
+};
+
+export const UpdateImgUrlProps: ModalBaseTypes = {
+  modalName: 'UpdateImgUrl',
+  dimentions: ['320px', '300px'],
+  title: 'Update picture',
+  text: 'You can add any picture',
+};
+
+export const ShowCategoryDetailsProps: ModalBaseTypes = {
+  modalName: 'ShowCategoryDetails',
+  dimentions: ['320px', '300px'],
+  title: 'Details',
+  text: 'You can edit any field?',
+};
---
+++ src/types/modalTypes.ts
@@ -0,0 +1,6 @@
+export interface ModalBaseTypes {
+    modalName: string;
+    dimentions: string[];
+    title: string;
+    text: string;
+  }
