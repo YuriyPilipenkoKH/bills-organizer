b9bf2550477b1b8e5d42f8c48a451a36e6b17086
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-06 15:52:05 +0200

    append ids

--- src/components/forms/DeleteBillForm.tsx
+++ src/components/forms/DeleteBillForm.tsx
@@ -1,17 +1,27 @@
 import React from 'react'
+
 interface DeleteBillFormProps {
-  id: string;
-  name: string
+  collectionId: string;
+  billId: string
   setIsSubmitting: React.Dispatch<React.SetStateAction<boolean>>;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
   }
 
   const DeleteBillForm: React.FC<DeleteBillFormProps> = ({ 
-    id, 
-    name,
+    collectionId, 
+    billId,
     setIsSubmitting,
     setOpen
    }) => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      setIsSubmitting(true)
+      const formData = new FormData();
+      formData.append('collectionId', collectionId);
+      formData.append('billId', billId);
+
+
+    }
   return (
     <div>DeleteBillForm</div>
   )
