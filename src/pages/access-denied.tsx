9e883f70a3acd872605077e38738929260b52105
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-10 18:59:25 +0200

    AccessDenied page added

---
+++ src/pages/access-denied.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AccessDenied() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Access Denied</h1>
+      <p>You do not have permission to view this page.</p>
+    </div>
+  );
+}
